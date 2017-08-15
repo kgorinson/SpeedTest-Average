ee3ee0fb09609aa1ba818bc8d5860f20d429e44a
kgor93 <mossme89@yahoo.com>
2017-08-15 08:17:23 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,7 +9,8 @@ rm -rf results.txt
 while true;
     do
         echo "Running speedtest now. Please wait..."
-        speedtest --simple --bytes | sed -e 's|,||g' >> results.txt
+        #speedtest --simple --bytes | sed -e 's|,||g' >> results.txt
+        ./speedtest.sh | sed -e 's|,||g' >> results.txt
         #awk '{s+=$3}END{print "ave:",s/NR}' RS=" " results.txt
         ping=$(cat results.txt | awk '{print $3}' | jq -s 'add/length')
         download=$(cat results.txt | awk '{print $4}' | jq -s 'add/length')
